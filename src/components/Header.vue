8da299eaf9a5268fabfad2beb37cdd83cb327555
Marco Tessitore <marcotexcode@gmail.com>
2021-07-16 13:13:06 +0200

    lavoro menu

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,7 +2,23 @@
   
     <div class="container">
 
-        <h2>Header</h2>
+        <img class="container--image" src="@/assets/img/medical_logo_1x_light.png" alt="">
+
+        <ul class="container--menu">
+            
+            <li class="container--menu--list" v-for="(link, index) in links" :key="index">
+                
+                <a class="container--menu--list--items"  :class="link.type == true ? 'active' : ' ' " :href="link.url">{{link.text}}</a>
+           
+            </li>
+
+            <li class="container--menu--list">
+
+                <a class="container--list--btn" href="#">make appointment</a>
+
+            </li>
+
+        </ul>
 
     </div>
 
@@ -11,9 +27,22 @@
 
 <script>               
 
+        import MenuItems from '@/data/MenuItems.js';
+
+
     export default {
         
-        name: 'Header'
+        name: 'Header',
+
+        data() {
+
+            return {
+
+                links: MenuItems
+
+            }
+
+        }
 
     }
 
@@ -24,6 +53,41 @@
 
     @import '@/style/variabili.scss';
     @import '@/style/mixins.scss';
+
+    .container {
+       padding: 20px;
+       display: flex;
+       align-items: center;
+       justify-content: space-between;
+
+       .container--image {
+           width: 200px;
+       }
+
+       .container--menu {
+           display: flex;
+
+           .container--menu--list {
+               margin: 0px 10px;
+               list-style: none;
+
+                .container--menu--list--items {
+                    color: #ffffff;
+                    text-decoration: none;
+                    text-transform: uppercase;
+                }
+
+            }
+
+            .container--list--btn {
+
+                @include btnPelorous();
+                    
+            }
+
+        }
+
+    }
 
 
 
---
+++ src/data/MenuItems.js
@@ -0,0 +1,31 @@
+
+export default [
+
+    {
+        text: 'home',
+        url: '#',
+        type: true
+    },
+
+    {
+        text: 'about',
+        url: '#',
+        type: false
+    },
+
+    {
+        text: 'departments',
+        url: '#',
+        type: false
+    },
+
+    {
+        text: 'articles',
+        url: '#',
+        type: false
+    }
+
+]
+
+    
+
